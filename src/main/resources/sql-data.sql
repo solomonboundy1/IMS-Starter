bc7ea8a197399c26b8030b2df7b67f41c73a4f59
solomonboundy1 <solomonboundy2017@gmail.com>
2021-10-21 16:24:25 +0100

    IMS-5 #database created

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,22 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+
+CREATE DATABASE ims;
+USE ims;
+
+CREATE TABLE Customer(
+	id int auto_increment,
+    firstName varchar(20) NOT NULL,
+    surname varchar(40) NOT NULL,
+    PRIMARY KEY (id)
+    );
+    
+CREATE TABLE Item (
+	itemId int auto_increment,
+	name varchar(40) NOT NULL,
+	price int NOT NULL,
+	PRIMARY KEY (itemId)
+	);
+	
+CREATE TABLE Order (
+	orderId int auto_increment,
+	

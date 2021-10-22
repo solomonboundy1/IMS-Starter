67a952052778af4b8e76199590ddf5fea270044e
solomonboundy1 <solomonboundy2017@gmail.com>
2021-10-22 11:56:05 +0100

    IMS-7 IMS-8 #comment database populated with required tables

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -2,4 +2,40 @@ package com.qa.ims.persistence.domain;
 
 public class Order {
 	
+	Customer CustomerId;
+	int itemQuantity;
+	float total;
+	public Order(Customer customerId, int itemQuantity, float total) {
+		CustomerId = customerId;
+		this.itemQuantity = itemQuantity;
+		this.total = total;
+	}
+	public Order(int itemQuantity, float total) {
+		this.itemQuantity = itemQuantity;
+		this.total = total;
+	}
+	public Customer getCustomerId() {
+		return CustomerId;
+	}
+	public void setCustomerId(Customer customerId) {
+		CustomerId = customerId;
+	}
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+	public float getTotal() {
+		return total;
+	}
+	public void setTotal(float total) {
+		this.total = total;
+	}
+	@Override
+	public String toString() {
+		return "Order [CustomerId=" + CustomerId + ", itemQuantity=" + itemQuantity + ", total=" + total + "]";
+	}
+	
+	
 }
--- src/main/java/com/qa/ims/persistence/domain/Orders_Items.java
+++ src/main/java/com/qa/ims/persistence/domain/Orders_Items.java
@@ -1,5 +1,42 @@
 package com.qa.ims.persistence.domain;
 
 public class Orders_Items {
+	
+	int ordersItemsId;
+	Order orderId;
+	Item itemId;
+	public Orders_Items(int ordersItemsId, Order orderId, Item itemId) {
+		this.ordersItemsId = ordersItemsId;
+		this.orderId = orderId;
+		this.itemId = itemId;
+	}
+	public Orders_Items(Order orderId, Item itemId) {
+		this.orderId = orderId;
+		this.itemId = itemId;
+	}
+	public int getOrdersItemsId() {
+		return ordersItemsId;
+	}
+	public void setOrdersItemsId(int ordersItemsId) {
+		this.ordersItemsId = ordersItemsId;
+	}
+	public Order getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Order orderId) {
+		this.orderId = orderId;
+	}
+	public Item getItemId() {
+		return itemId;
+	}
+	public void setItemId(Item itemId) {
+		this.itemId = itemId;
+	}
+	@Override
+	public String toString() {
+		return "Orders_Items [ordersItemsId=" + ordersItemsId + ", orderId=" + orderId + ", itemId=" + itemId + "]";
+	}
+	
+	
 
 }
